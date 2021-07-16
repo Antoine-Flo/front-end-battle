7957db883e878bcfd4e0dae5566f87a55ab54869
Antoine-Flo <antoine.flouzat@gmail.com>
2021-07-16 22:29:33 +0200

    challenges created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PlayComponent } from './modules/game/pages/play/play.component';
+import { ChallengesComponent } from './modules/home/components/challenges/challenges.component';
 import { HomeComponent } from './modules/home/pages/home/home.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
-    path: '',
+    path: 'home',
     component: HomeComponent,
     children: [
-      {
-        path: 'game',
-        component: PlayComponent
-      },
+      { path: 'game', component: PlayComponent },
+      { path: '', component: ChallengesComponent },
     ],
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
