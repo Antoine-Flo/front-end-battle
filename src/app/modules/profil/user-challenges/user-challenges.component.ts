caf97e15eb00d8a96e1ffe6730e8530b07972b30
Antoine-Flo <antoine.flouzat@gmail.com>
2021-08-26 13:04:06 +0200

    collect user profil ok

--- src/app/modules/profil/user-challenges/user-challenges.component.ts
+++ src/app/modules/profil/user-challenges/user-challenges.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { forkJoin } from 'rxjs';
-import { concatMap, mergeMap, pluck, tap } from 'rxjs/operators';
+import { forkJoin, from, of } from 'rxjs';
+import { concatMap, map, mergeMap, pluck, tap } from 'rxjs/operators';
 import { AuthService } from 'src/app/auth/auth.service';
 import { ChallengeService } from 'src/app/core/services/challenge.service';
 import { UserService } from 'src/app/core/services/user.service';
@@ -11,8 +11,6 @@ import { UserService } from 'src/app/core/services/user.service';
   styleUrls: ['./user-challenges.component.scss'],
 })
 export class UserChallengesComponent implements OnInit {
-
- 
   userEmail: string;
   userChallengesIds: string[];
   userChallenges: any;
@@ -24,19 +22,19 @@ export class UserChallengesComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-
     this.userEmail = this.authService.getUserEmail();
 
-
-    this.userService.getOne(this.userEmail).pipe(
-
-      pluck('challenges'),
-
-    ).subscribe((result) => this.userChallengesIds = result);
-
-    console.log(this.userChallengesIds);
-    
-    // this.userChallenges = this.challengesService.getSome(this.userChallengesIds)
+    this.userService
+      .getOne(this.userEmail)
+      .pipe(
+        pluck('challenges'),
+        mergeMap((arr) =>
+          forkJoin(arr.map((id) => this.challengesService.getOne(id)))
+        )
+      )
+      .subscribe((result) => {
+        this.userChallenges = result;
+      });
   }
 
   onChallengeDeleted() {
