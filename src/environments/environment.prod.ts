8403a4abc9a8bfaaa651c8e8eaf4694b60984fb9
Antoine-Flo <antoine.flouzat@gmail.com>
2021-09-01 18:37:35 +0200

    test env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,14 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyA8shl3xSB0eoAn6UW8yLusXYLlrTvP1AA',
+    authDomain: 'front-end-battle.firebaseapp.com',
+    projectId: 'front-end-battle',
+    storageBucket: 'front-end-battle.appspot.com',
+    messagingSenderId: '702641835673',
+    appId: '1:702641835673:web:2bd3fed247feb568813208',
+    measurementId: 'G-CVW1DLM3XN',
+  },
   api: {
     challenges: 'https://feb-api.com/challenges',
     users: 'https://feb-api.com/users'
